fa1f08f9ebef0a9ec8d50dcc517d9ece075d7960
Conrad Etherington <conrad.etherington@gmail.com>
2022-12-15 18:29:12 -0700

    commit tree b438256df458e991a6699a362e60d5d3c37b1733

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
-  def index
+  def index 
+    if params[:name] != nil
+
+      students = Student.where("lower(first_name) = lower(?)", params[:name]).or(Student.where("lower(last_name) = lower(?)", params[:name] ))
+      render json: students
+
+    else
     students = Student.all
     render json: students
+
+    end
   end
+  def show
+    student = Student.find params[:id]
+    render json: student
+   end
 
 end
